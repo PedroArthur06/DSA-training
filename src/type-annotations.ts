f2740c461ee5f308df4747f541cb538921686cf3
Pedro Arthur <pedroxxl917@gmail.com>
2026-05-02 14:00:43 -0400

    feat: add basic TypeScript type annotation examples and functions

---
+++ src/type-annotations.ts
@@ -0,0 +1,17 @@
+const name: string = "Pedro Basto"
+
+const age: number = 19
+
+const jaTrabalha: boolean = true
+
+function sum(a: number, b:number) : number{
+    return a + b
+}
+
+sum(10, 5)
+
+function concatena(name: string, lastName: string): string{
+    return name + lastName
+}
+
+concatena("Pedro", "Arthur")
